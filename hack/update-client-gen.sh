12f33f7f0e4208f01ff1f93828cc9fbec352c1e0
Luke Addison <lukeaddison785@gmail.com>
2017-10-16 01:19:30 +0100

    Generate code

--- hack/update-client-gen.sh
+++ hack/update-client-gen.sh
@@ -13,28 +13,28 @@ BINDIR=${REPO_ROOT}/bin
 ${BINDIR}/client-gen "$@" \
         --input-base "github.com/dippynark/kube-qemu/pkg/apis/" \
         --input "hypervisor/" \
-        --clientset-path "github.com/dippynark/kube-qemu/pkg/client/" \
-        --clientset-name internalclientset \
+        --clientset-path "github.com/dippynark/kube-qemu/pkg/client/clientset_generated/" \
+        --clientset-name "internalclientset" \
         --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # Generate the versioned clientset (pkg/client/clientset_generated/clientset)
 ${BINDIR}/client-gen "$@" \
         --input-base "github.com/dippynark/kube-qemu/pkg/apis/" \
         --input "hypervisor/v1alpha1" \
-        --clientset-path "github.com/dippynark/kube-qemu/pkg/" \
-        --clientset-name "client" \
+        --clientset-path "github.com/dippynark/kube-qemu/pkg/client/clientset_generated/" \
+        --clientset-name "clientset" \
         --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # generate lister
 ${BINDIR}/lister-gen "$@" \
 	--input-dirs="github.com/dippynark/kube-qemu/pkg/apis/hypervisor" \
         --input-dirs="github.com/dippynark/kube-qemu/pkg/apis/hypervisor/v1alpha1" \
-        --output-package "github.com/dippynark/kube-qemu/pkg/listers" \
+        --output-package "github.com/dippynark/kube-qemu/pkg/client/listers_generated" \
         --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt"
 # generate informer
 ${BINDIR}/informer-gen "$@" \
         --go-header-file "${GOPATH}/src/github.com/kubernetes/repo-infra/verify/boilerplate/boilerplate.go.txt" \
         --input-dirs "github.com/dippynark/kube-qemu/pkg/apis/hypervisor" \
         --input-dirs "github.com/dippynark/kube-qemu/pkg/apis/hypervisor/v1alpha1" \
-        --internal-clientset-package "github.com/dippynark/kube-qemu/pkg/client/internalclientset" \
-        --versioned-clientset-package "github.com/dippynark/kube-qemu/pkg/client" \
-        --listers-package "github.com/dippynark/kube-qemu/pkg/listers" \
-        --output-package "github.com/dippynark/kube-qemu/pkg/informers"
+        --internal-clientset-package "github.com/dippynark/kube-qemu/pkg/client/clientset_generated/internalclientset" \
+        --versioned-clientset-package "github.com/dippynark/kube-qemu/pkg/client/clientset_generated/clientset" \
+        --listers-package "github.com/dippynark/kube-qemu/pkg/client/listers_generated" \
+        --output-package "github.com/dippynark/kube-qemu/pkg/client/informers_generated"
